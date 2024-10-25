0436201b7b2148fd68509c99caf80017c1c87aa2
hrithwik-s-sukumar <hrithwikhricha@gmail.com>
2024-10-25 16:09:30 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,6 @@ import random
 from django.core.mail import send_mail
 from django.conf import settings
 
-
-
-# Create your views here.
 def user_register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
